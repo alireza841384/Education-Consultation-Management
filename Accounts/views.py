007218d415cff83e4291e750dde4d3aace38aa2c
Parsa313-cloud <haroonip313@gmail.com>
2026-07-05 23:47:31 +0330

    feat(accounts): implement register, login and profile endpoints

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.throttling import AnonRateThrottle
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import UserRegisterSerializer, UserSerializer
 
-# Create your views here.
+class RegisterView(CreateAPIView):
+    serializer_class = UserRegisterSerializer
+    permission_classes = [AllowAny]
+    throttle_classes = [AnonRateThrottle]
+
+
+class LoginView(TokenObtainPairView):
+    permission_classes = [AllowAny]
+    throttle_classes = [AnonRateThrottle] 
+  
+class MeView(RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user

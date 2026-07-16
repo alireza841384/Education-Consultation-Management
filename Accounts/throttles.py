2ada7c8e7450ab2c023f9f6c6198de606d10fc76
Parsa313-cloud <haroonip313@gmail.com>
2026-07-16 23:29:03 +0330

    feat(account): Add custom throttles for login and registration

---
+++ Accounts/throttles.py
@@ -0,0 +1,9 @@
+from rest_framework.throttling import AnonRateThrottle
+
+
+class LoginThrottle(AnonRateThrottle):
+    rate = "5/min"
+
+
+class RegisterThrottle(AnonRateThrottle):
+    rate = "10/hour"
--- Accounts/views.py
+++ Accounts/views.py
@@ -1,18 +1,18 @@
 from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from rest_framework.throttling import AnonRateThrottle
+from .throttles import LoginThrottle, RegisterThrottle
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import UserRegisterSerializer, UserSerializer
 
 class RegisterView(CreateAPIView):
     serializer_class = UserRegisterSerializer
     permission_classes = [AllowAny]
-    throttle_classes = [AnonRateThrottle]
+    throttle_classes = [RegisterThrottle]
 
 
 class LoginView(TokenObtainPairView):
     permission_classes = [AllowAny]
-    throttle_classes = [AnonRateThrottle] 
+    throttle_classes = [LoginThrottle] 
   
 class MeView(RetrieveUpdateAPIView):
     serializer_class = UserSerializer

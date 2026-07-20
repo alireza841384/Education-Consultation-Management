3a743fd06d7c24d17527435c8514d764d1cb5ae3
Parsa313-cloud <haroonip313@gmail.com>
2026-07-20 22:19:01 +0330

    feat(reporting): implement report viewset

--- Reporting/views.py
+++ Reporting/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.generics import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from Accounts.models import CustomUser
+from .serializers import ReportingSerializer
+from .models import Report
+
+class ReportingViewSet(ModelViewSet):
+    serializer_class = ReportingSerializer
+    permission_classes = [IsAuthenticated]
+    query_set = Report.objects.all()
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.type == CustomUser.Types.ADMIN:
+            return Report.objects.all()
+        return Report.objects.filter(user=user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)

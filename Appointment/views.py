82bcbab5734ffd46a5acf57be4b89f6c4b81f614
Parsa313-cloud <haroonip313@gmail.com>
2026-07-26 21:14:13 +0330

    feat(appointment) : implement view for Schedule

--- Appointment/views.py
+++ Appointment/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from Accounts.models import CustomUser
 
-# Create your views here.
+from .serializers import AppointmentSerializer , ScheduleSerializer, AppointmentSlotSerializer
+from .models import Appointment, Schedule, AppointmentSlot
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+class ScheduleViewSet(ModelViewSet):
+    serializer_class = ScheduleSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = Schedule.objects.all()
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.type == CustomUser.Types.ADMIN:
+            return Schedule.objects.all()
+        return Schedule.objects.filter(advisor=user)
+    
+    def perform_create(self, serializer):
+        serializer.save(advisor=self.request.user)

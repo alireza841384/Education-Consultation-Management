d2f31d08afb5df30e5a32c90e361e5df105a5def
Parsa313-cloud <haroonip313@gmail.com>
2026-07-26 22:52:07 +0330

    refactor(appointment) : refactor the view of schedule

--- Accounts/permissions.py
+++ Accounts/permissions.py
@@ -14,3 +14,19 @@ class IsOwnerOrAdmin(BasePermission):
         if request.user.type == CustomUser.Types.ADMIN:
             return True
         return obj.user == request.user
+
+class IsAdvisorOwner(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return request.user.is_authenticated
+
+        return (
+            request.user.is_authenticated
+            and request.user.type == CustomUser.Types.ADMIN
+        )
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+
+        return obj.advisor == request.user
--- Appointment/views.py
+++ Appointment/views.py
@@ -4,17 +4,25 @@ from .serializers import AppointmentSerializer , ScheduleSerializer, Appointment
 from .models import Appointment, Schedule, AppointmentSlot
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
+from Accounts.permissions import IsAdvisorOwner
 
 class ScheduleViewSet(ModelViewSet):
     serializer_class = ScheduleSerializer
-    permission_classes = [IsAuthenticated]
-    queryset = Schedule.objects.all()
+    permission_classes = [IsAuthenticated, IsAdvisorOwner]
 
     def get_queryset(self):
         user = self.request.user
+        
         if user.type == CustomUser.Types.ADMIN:
-            return Schedule.objects.all()
-        return Schedule.objects.filter(advisor=user)
-    
+            return Schedule.objects.filter(
+                advisor=user,
+            )
+
+        return Schedule.objects.filter(
+            advisor=user.profile.advisor,
+        )
+
     def perform_create(self, serializer):
-        serializer.save(advisor=self.request.user)
+        serializer.save(
+            advisor=self.request.user,
+        )
